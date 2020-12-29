891ecfe13016745ce1fbddda3e00478295d183d5
Inge Huijsmans <ihuijsmans@hotmail.com>
2020-12-29 16:18:31 +0100

    Update costFunctions.py
    remove resid_share

--- analyses/computation/costFunctions.py
+++ analyses/computation/costFunctions.py
@@ -2,7 +2,7 @@ import numpy as np
 from inequalityaversion import two_norms_model
 
 
-def two_norms_costfun(param,subDat,printStep=False,printPredictions=False,resid_share=False):
+def two_norms_costfun(param,subDat,printStep=False,printPredictions=False):
     theta = param[0]
     phi = param[1]
 
@@ -11,9 +11,6 @@ def two_norms_costfun(param,subDat,printStep=False,printPredictions=False,resid_
             subDat.loc[trial,'BP1'],
             subDat.loc[trial,'BP2'],
             theta, phi)
-    if resid_share == True:
-        residuals = (subDat.loc[:,'ret'] - subDat.loc[:,'prediction'])/(subDat.loc[:,'inv'] * subDat.loc[:,'mult'])
-    else:
         residuals = subDat.loc[:,'choice'] - subDat.loc[:,'prediction']
     
     residuals = residuals.astype('float')
